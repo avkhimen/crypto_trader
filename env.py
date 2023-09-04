d2a43f7f4cf12497b54dc68c995d6ae249e68c81
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-09-04 00:25:48 -0600

    Adding notes and env file

---
+++ env.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+class CryptoEnv()
+    def __init__(self, price_series):
+        self.price_series = price_series
+    def reset(self):
+        pass
+    def step(self):
+        pass
---
+++ notes.md
@@ -0,0 +1,5 @@
+# Possible Approaches
+
+## Ideas for Processing
+
+1. 
