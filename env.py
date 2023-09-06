e5581fd3d3f74435d8627c4acacd661c21688793
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-09-05 22:49:59 -0600

    Selecting random index from price series

--- env.py
+++ env.py
@@ -5,6 +5,8 @@ class CryptoEnv()
     def __init__(self, all_price_series):
         self.all_price_series = all_price_series
     def reset(self):
+        self.price_size = self.all_price_series.shape[0
+        random_index = np.random.choice(self.price_size, 1)
         pass
     def step(self):
         pass
