a802a43b6af9b1232ef8355adbec7c6fce8cf5c7
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-09-05 22:48:00 -0600

    Adding idea and implementing in env class on how price series will propagate inside the class

--- env.py
+++ env.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 
 class CryptoEnv()
-    def __init__(self, price_series):
-        self.price_series = price_series
+    def __init__(self, all_price_series):
+        self.all_price_series = all_price_series
     def reset(self):
         pass
     def step(self):
--- notes.md
+++ notes.md
@@ -4,4 +4,6 @@
 
 Appraach 1:
 
-1.  
+1. The environment will be initialized with the entirety of the price series
+2. The environment will randonly select the required price series
+3. The training will take place on the price series selected by the model 
