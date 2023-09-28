966ad84bd5a3f5d45c624b043937a5887857ba02
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-09-27 22:51:50 -0600

    Adding train and scoring steps to main.py

--- main.py
+++ main.py
@@ -3,47 +3,58 @@ from env import CryptoEnv
 from agents.dqn import dqn
 import pandas as pd
 from data_utils.support_functions import load_ts
-from agents.dqn import ReplayBuffer
+from agents.dqn import ReplayBuffer, Qnet
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+from agents.dqn import train
+
+learning_rate = 0.0005
+gamma         = 0.98
+buffer_limit  = 50000
+batch_size    = 32
 
 def main():
     ts = load_ts('close')
     # Step 1: Create and configure the environment
     env = CryptoEnv("YourEnvName")  # Replace "YourEnvName" with the name of your Gym environment
-    state_dim = env.observation_space.shape[0]
-    action_dim = env.action_space.shape[0]
+    q = Qnet()
+    q_target = Qnet()
+    q_target.load_state_dict(q.state_dict())
     memory = ReplayBuffer()
-    # Step 2: Initialize the SAC agent
-    agent = SAC(state_dim, action_dim,  # Specify state and action dimensions
-                actor_lr=0.001, critic_lr=0.001, alpha_lr=0.001)  # Tune hyperparameters
+    print_interval = 20
+    score = 0.0  
+    optimizer = optim.Adam(q.parameters(), lr=learning_rate)
 
-    # Step 3: Training loop
-    max_episodes = 1000  # Adjust as needed
-    for episode in range(max_episodes):
-        state = env.reset()
+    for n_epi in range(10000):
+        epsilon = max(0.01, 0.08 - 0.01*(n_epi/200)) #Linear annealing from 8% to 1%
+        s = env.reset()
         done = False
-        total_reward = 0
 
         while not done:
-            # Step 4: Select an action from the agent's policy
-            action = agent.select_action(state)
+            a = q.sample_action(torch.from_numpy(s).float(), epsilon)      
+            s_prime, r, done, info = env.step(a)
+            done_mask = 0.0 if done else 1.0
+            memory.put((s,a,r,s_prime, done_mask))
+            s = s_prime
 
-            # Step 5: Take a step in the environment
-            next_state, reward, done, _ = env.step(action)
+            score += r
+            if done:
+                break
 
-            # Step 6: Store the transition in the replay buffer
-            agent.replay_buffer.add(state, action, reward, next_state, done)
+        if memory.size()>2000:
+            train(q, q_target, memory, optimizer)
 
-            # Step 7: Train the agent
-            agent.train()
+        if n_epi%print_interval==0 and n_epi!=0:
+            q_target.load_state_dict(q.state_dict())
+            print("n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
+                                                            n_epi, score/print_interval, memory.size(), epsilon*100))
+            score = 0.0
+        
+    env.close()
 
-            # Update the current state and total reward
-            state = next_state
-            total_reward += reward
-
-        print(f"Episode {episode + 1} - Total Reward: {total_reward}")
-
-    # Step 8: Save the trained model if needed
-    agent.save_model("sac_model.pth")
 
 if __name__ == '__main__':
     main()
