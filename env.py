2bf2f37a56e3582997f32fbcb96572823ca0c20c
Vadim Avkhimenia <avkhimen@ualberta.ca>
2023-09-29 22:08:45 -0600

    Correcting series

--- env.py
+++ env.py
@@ -1,8 +1,7 @@
 import numpy as np
-import pandas as pd
 
 class CryptoEnv():
-    def __init__(self, all_price_series, lookup_interval=1, window_size=2):
+    def __init__(self, all_price_series, lookup_interval, window_size):
         self.all_price_series = all_price_series
         self.window_size = window_size
         self.lookup_interval = lookup_interval
@@ -14,15 +13,14 @@ class CryptoEnv():
     def select_random_window(self):
         """Select a window from the array with a random starting index."""
         if self.window_size > len(self.all_price_series):
-            return []
+            raise Exception("Please choose a larger window size")
         # Choose a random starting index
-        start_index = np.random.randint(0, len(self.all_price_series) - self.window_size + 1)
-        self.ser = np.array(self.all_price_series[start_index - self.lookup_interval:start_index + self.window_size])
-        self.ser = self.normalize_price_series()
-        
-        return self.ser.tolist()
-    def normalize_price_series(self):
-        return self.ser/self.ser[0]
+        self.start_index = np.random.randint(0, len(self.all_price_series) - 100)
+        self.ser = np.array(self.all_price_series[self.start_index:self.start_index+self.window_size+self.lookup_interval+1])
+        self.ser_normalized = (np.array(self.ser)/self.ser[0]).tolist()
+        print(self.ser_normalized)
+        print(len(self.ser_normalized))
+        return self.ser_normalized[:self.lookup_interval]
     def step(self, state, action):
         cash_state = state[0]
         if cash_state == 0:
@@ -30,29 +28,32 @@ class CryptoEnv():
                 reward = 0
                 next_cash_state = 0
             elif action == 1: # buy
-                reward = self.ser[self.step_order + 1] -  self.ser[self.step_order]
+                reward = self.ser_normalized[self.lookup_interval + self.step_order + 1] - self.ser_normalized[self.lookup_interval + self.step_order]
                 next_cash_state = 1
             elif action == 2: # sell
                 reward = 0
                 next_cash_state = 0
         elif cash_state == 1:
             if action == 0: # do nothing
-                reward = self.ser[self.step_order + 1] -  self.ser[self.step_order]
+                print('First index', self.lookup_interval + self.step_order + 1)
+                reward = self.ser_normalized[self.lookup_interval + self.step_order + 1] - self.ser_normalized[self.lookup_interval + self.step_order]
                 next_cash_state = 1
             elif action == 1: # buy
                 reward = 0
                 next_cash_state = 1
             elif action == 2: # sell
-                if self.ser[self.step_order + 1] -  self.ser[self.step_order] < 0:
+                if self.ser_normalized[self.lookup_interval + self.step_order + 1] - self.ser_normalized[self.lookup_interval + self.step_order] < 0:
                     reward = 0
                 else:
-                    reward = -(self.ser[self.step_order + 1] -  self.ser[self.step_order])
+                    reward = -(self.ser_normalized[self.lookup_interval + self.step_order + 1] - self.ser_normalized[self.lookup_interval + self.step_order])
                 next_cash_state = 0
         next_state = [next_cash_state] # must be a list
-        next_state.extend()
+        self.step_order += 1
+        self.ser = np.array(self.all_price_series[self.start_index+self.step_order:self.start_index+self.lookup_interval+self.step_order])
+        self.ser_normalized = (np.array(self.ser)/self.ser[0]).tolist()
+        next_state.extend(self.ser_normalized)
         done = False
         if self.step_order == self.window_size:
             done = True
         info = {}
-        self.step_order += 1
         return next_state, reward, done, info
--- main.py
+++ main.py
@@ -1,25 +1,50 @@
+import sys
+
+sys.path.append('agents/')
+
 # Method to run the entire training process
 from env import CryptoEnv
-from agents.dqn import dqn
 import pandas as pd
 from data_utils.support_functions import load_ts
-from agents.dqn import ReplayBuffer, Qnet
+from dqn import ReplayBuffer, train
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import random
-from agents.dqn import train
+import numpy as np
 
 learning_rate = 0.0005
 gamma         = 0.98
 buffer_limit  = 50000
 batch_size    = 32
 
+class Qnet(nn.Module):
+    def __init__(self):
+        super(Qnet, self).__init__()
+        self.fc1 = nn.Linear(4, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 2)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+      
+    def sample_action(self, obs, epsilon):
+        out = self.forward(obs)
+        coin = random.random()
+        if coin < epsilon:
+            return random.randint(0,1)
+        else : 
+            return out.argmax().item()
+
 def main():
-    ts = load_ts('close')
+    ts = load_ts('close_price')
     # Step 1: Create and configure the environment
-    env = CryptoEnv(ts)
+    env = CryptoEnv(ts, lookup_interval=3, window_size=6)
+    q = Qnet()
     q_target = Qnet()
     q_target.load_state_dict(q.state_dict())
     memory = ReplayBuffer()
@@ -30,17 +55,19 @@ def main():
     for n_epi in range(10000):
         epsilon = max(0.01, 0.08 - 0.01*(n_epi/200)) #Linear annealing from 8% to 1%
         s = env.reset()
+        print('reset state')
         done = False
 
         while not done:
-            a = q.sample_action(torch.from_numpy(s).float(), epsilon)      
-            s_prime, r, done, info = env.step(a)
+            a = q.sample_action(torch.from_numpy(np.array(s)).float(), epsilon)      
+            s_prime, r, done, info = env.step(s, a)
             done_mask = 0.0 if done else 1.0
             memory.put((s,a,r,s_prime, done_mask))
             s = s_prime
 
             score += r
             if done:
+                print('Episode completed')
                 break
 
         if memory.size()>2000:
